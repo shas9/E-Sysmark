7515200615836903f77832f9584e089f50e11b5d
RafeedMohammad <rafeedmohammad1107@gmail.com>
2023-04-19 01:41:14 +0600

    Database connection added

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
     res.send("Welcome to E-Sysmark");
 });
 
+mongoose
+  .connect("mongodb://127.0.0.1:27017/eSysmark")
+  .then(()=> console.log("Connected to mongo db"))
+  .catch((err) => console.log(err.message));
 
 
 const port = process.env.PORT || 3000;
