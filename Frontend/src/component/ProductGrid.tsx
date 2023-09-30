fb55948eb1f61cce6ea2a35c069df7389b493435
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-10-01 02:23:21 +0600

    Added shadow around the product grid

--- Frontend/src/component/ProductGrid.tsx
+++ Frontend/src/component/ProductGrid.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, Text, Box } from "@chakra-ui/react";
+import { SimpleGrid, Text, Box, useColorModeValue } from "@chakra-ui/react";
 import useProducts from "../hooks/useProducts";
 import ProductCard from "./ProductCard";
 import ProductCardSkeleton from "./ProductCardSkeleton";
@@ -13,8 +13,18 @@ const ProductGrid = ({ selectedCategory }: Props) => {
   const { data, error, isLoading } = useProducts(selectedCategory);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
 
+  // Determine the shadow color based on the color mode (light/dark)
+  const shadowColor = useColorModeValue(
+    "rgba(0, 0, 0, 0.1)",
+    "rgba(255, 255, 255, 0.1)"
+  );
+
   return (
-    <Box>
+    <Box
+      boxShadow={`0px 0px 6px 2px ${shadowColor}`} // Add shadow to all sides
+      borderRadius="md"
+      p={4}
+    >
       {error && (
         <Text color="red.500" fontSize="xl" textAlign="center" my={4}>
           {error}
@@ -22,7 +32,7 @@ const ProductGrid = ({ selectedCategory }: Props) => {
       )}
 
       {isLoading ? (
-        <SimpleGrid columns={{ sm: 1, md: 3, lg: 3, xl: 5 }} spacing={4} p={4}>
+        <SimpleGrid columns={{ sm: 1, md: 3, lg: 3, xl: 5 }} spacing={4}>
           {skeletons.map((skeleton) => (
             <ProductCardContainer key={skeleton}>
               <ProductCardSkeleton />
@@ -30,7 +40,7 @@ const ProductGrid = ({ selectedCategory }: Props) => {
           ))}
         </SimpleGrid>
       ) : (
-        <SimpleGrid columns={{ sm: 1, md: 3, lg: 3, xl: 5 }} spacing={4} p={4}>
+        <SimpleGrid columns={{ sm: 1, md: 3, lg: 3, xl: 5 }} spacing={4}>
           {data.map((product) => (
             <ProductCardContainer key={product.id}>
               <ProductCard product={product} />
