670fba63112d207e2e9d10132b56aeee43048742
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-10-01 04:18:25 +0600

    Updated the navbar design

--- Frontend/src/component/NavBar.tsx
+++ Frontend/src/component/NavBar.tsx
@@ -1,12 +1,29 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  Text,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import logo from "../assets/Sysmark-logo.png";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
+  const { colorMode } = useColorMode();
+
+  // Define colors and styles for both light and dark modes
+  const bgColor = useColorModeValue("gray.200", "gray.900"); // Background color
+  const boxShadow = useColorModeValue("md", "none"); // Box shadow
+
   return (
-    <HStack justifyContent="space-between" padding="10px">
+    <HStack
+      justifyContent="space-between"
+      padding="10px" // Add padding here (adjust as needed)
+      bg={bgColor}
+      boxShadow={boxShadow}
+    >
       <Image src={logo} boxSize="60px" m={2} />
-      <ColorModeSwitch></ColorModeSwitch>
+      <ColorModeSwitch />
     </HStack>
   );
 };
