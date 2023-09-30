8d6ad0e2eabedd6344587025a7d596a0419f679a
Shahwat Hasnaine <shahwathasnaine@gmail.com>
2023-10-01 02:14:47 +0600

    Updated Grid and Card design

--- Frontend/src/component/ProductCard.tsx
+++ Frontend/src/component/ProductCard.tsx
@@ -1,5 +1,5 @@
 import { Product } from "../hooks/useProducts";
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Box, Image, Text, useColorMode } from "@chakra-ui/react";
 import { getCroppedImageUrl } from "../services/image-url";
 
 interface Props {
@@ -7,13 +7,40 @@ interface Props {
 }
 
 const ProductCard = ({ product }: Props) => {
+  const { colorMode } = useColorMode();
+
+  // Define colors for light and dark modes
+  const bgColor = { light: "white", dark: "gray.700" };
+  const textColor = { light: "black", dark: "white" };
+  const borderColor = { light: "gray.200", dark: "gray.600" };
+
   return (
-    <Card>
-      <Image src={getCroppedImageUrl(product.background_image)}></Image>
-      <CardBody>
-        <Heading fontSize="2xl">{product.name}</Heading>
-      </CardBody>
-    </Card>
+    <Box
+      borderWidth="1px"
+      borderRadius="lg"
+      overflow="hidden"
+      boxShadow="md"
+      transition="transform 0.2s"
+      _hover={{ transform: "scale(1.02)" }}
+      bgColor={bgColor[colorMode]}
+      color={textColor[colorMode]}
+      borderColor={borderColor[colorMode]}
+    >
+      <Image
+        src={getCroppedImageUrl(product.background_image)}
+        alt={product.name}
+        height="200px"
+        objectFit="cover"
+      />
+      <Box p="4">
+        <Text fontSize="lg" fontWeight="semibold" mb="2">
+          {product.name}
+        </Text>
+        <Text fontSize="md" color="gray.600">
+          Price: <strong>{"product.price"}</strong>
+        </Text>
+      </Box>
+    </Box>
   );
 };
 
--- Frontend/src/component/ProductGrid.tsx
+++ Frontend/src/component/ProductGrid.tsx
@@ -1,4 +1,4 @@
-import { SimpleGrid, Text } from "@chakra-ui/react";
+import { SimpleGrid, Text, Box } from "@chakra-ui/react";
 import useProducts from "../hooks/useProducts";
 import ProductCard from "./ProductCard";
 import ProductCardSkeleton from "./ProductCardSkeleton";
@@ -12,27 +12,33 @@ interface Props {
 const ProductGrid = ({ selectedCategory }: Props) => {
   const { data, error, isLoading } = useProducts(selectedCategory);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
+
   return (
-    <>
-      {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 3, lg: 3, xl: 5 }}
-        padding="10px"
-        spacing={3}
-      >
-        {isLoading &&
-          skeletons.map((skeleton) => (
+    <Box>
+      {error && (
+        <Text color="red.500" fontSize="xl" textAlign="center" my={4}>
+          {error}
+        </Text>
+      )}
+
+      {isLoading ? (
+        <SimpleGrid columns={{ sm: 1, md: 3, lg: 3, xl: 5 }} spacing={4} p={4}>
+          {skeletons.map((skeleton) => (
             <ProductCardContainer key={skeleton}>
               <ProductCardSkeleton />
             </ProductCardContainer>
           ))}
-        {data.map((product) => (
-          <ProductCardContainer key={product.id}>
-            <ProductCard product={product} />
-          </ProductCardContainer>
-        ))}
-      </SimpleGrid>
-    </>
+        </SimpleGrid>
+      ) : (
+        <SimpleGrid columns={{ sm: 1, md: 3, lg: 3, xl: 5 }} spacing={4} p={4}>
+          {data.map((product) => (
+            <ProductCardContainer key={product.id}>
+              <ProductCard product={product} />
+            </ProductCardContainer>
+          ))}
+        </SimpleGrid>
+      )}
+    </Box>
   );
 };
 
